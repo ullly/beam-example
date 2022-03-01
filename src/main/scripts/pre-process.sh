e6aa653f0f42962672f7ba75c4b847b5edea1001
ullly <omorainu@tcd.ie>
2022-03-01 18:06:16 +0000

    Update formatting

--- src/main/scripts/pre-process.sh
+++ src/main/scripts/pre-process.sh
@@ -2,7 +2,7 @@
 
 if [ $1 = "1" ]
 then
-	# Make valid JSON
+  # Make valid JSON
   echo "[" > temp.txt
   cat src/main/java/resources/input.txt >> temp.txt
   echo "]" >> temp.txt
@@ -12,11 +12,11 @@ then
 
   $(rm temp.txt)
 else
-	 # Make valid JSON
-   sed -E 's/(steps |id|type|from|input|of|to|input)(:|=)/"\1":/g' src/main/java/resources/config.txt |
+  # Make valid JSON
+  sed -E 's/(steps |id|type|from|input|of|to|input)(:|=)/"\1":/g' src/main/java/resources/config.txt |
     sed -E 's/([a-zA-Z]*.class)/"\1"/g' | sed -E 's/("to".*),/\1/g' > temp.txt
 
-   cat temp.txt > src/main/java/resources/config.txt
+  cat temp.txt > src/main/java/resources/config.txt
 
-    $(rm temp.txt)
+  $(rm temp.txt)
 fi
