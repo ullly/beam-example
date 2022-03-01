a3fec6b4d13ba5a7c86723bb139a10f0cc7c649b
ullly <omorainu@tcd.ie>
2022-03-01 17:59:38 +0000

    Update script to accept config file

--- src/main/scripts/pre-process.sh
+++ src/main/scripts/pre-process.sh
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
-# Make valid JSON
-echo "[" > temp.txt
-cat src/main/java/resources/input.txt >> temp.txt
-echo "]" >> temp.txt
+if [ $1 = "1" ]
+then
+	# Make valid JSON
+  echo "[" > temp.txt
+  cat src/main/java/resources/input.txt >> temp.txt
+  echo "]" >> temp.txt
 
-# Convert to newline delimited JSON
-cat temp.txt | jq -c '.[]' > src/main/java/resources/input.txt
+  # Convert to newline delimited JSON
+  cat temp.txt | jq -c '.[]' > src/main/java/resources/input.txt
 
-$(rm temp.txt)
+  $(rm temp.txt)
+else
+	 # Make valid JSON
+   sed -E 's/(steps |id|type|from|input|of|to|input)(:|=)/"\1":/g' src/main/java/resources/config.txt |
+    sed -E 's/([a-zA-Z]*.class)/"\1"/g' | sed -E 's/("to".*),/\1/g' > temp.txt
+
+   cat temp.txt > src/main/java/resources/config.txt
+
+    $(rm temp.txt)
+fi
